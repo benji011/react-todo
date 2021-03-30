5ad59f3c944501af4a625df4c614dc9e0be9713f
benji011 <wolfnun011@gmail.com>
2021-03-30 16:52:50 +0900

    fix: cleanup

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,8 +3,14 @@ import { Link } from "react-router-dom";
 const About = () => {
   return (
     <div>
-      <h1>About this app</h1>
-      <Link to="/">Home</Link>
+      <h1 className="title">About</h1>
+      <p> simple todo app using ReactJS, Bulma CSS, Supabase & Vercel</p>
+      <p>
+        You can read more about it on GitHub{" "}
+        <a href="https://github.com/benji011/react-todo">here</a>
+      </p>
+      <br />
+      <Link to="/">Go back to home</Link>
     </div>
   );
 };
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,6 +8,7 @@ const Footer = () => {
         <a href="https://benjaminlo.io">Benjamin Lo</a>. The source code is
         licensed
         <a href="http://opensource.org/licenses/mit-license.php"> MIT. </a>
+        <br />
         <Link to="/about">About this app</Link>
       </p>
     </footer>
