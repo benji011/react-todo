e7df3f8edaecad16df8d20a8925cb9c050400f11
benji011 <wolfnun011@gmail.com>
2021-03-30 16:12:25 +0900

    fix: Added API key values in constants

---
+++ src/constants.tsx
@@ -0,0 +1,3 @@
+export const API_URL: string = process.env.REACT_APP_API_URL!;
+export const API_KEY: string = `apikey=${process.env.REACT_APP_API_KEY}`!;
+export const BEARER_TOKEN: string = `Bearer ${process.env.REACT_APP_API_KEY}`!;
