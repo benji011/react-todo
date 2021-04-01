8ced5e9ccc544e08fc8ee2357e29edd78190f5f0
benji011 <wolfnun011@gmail.com>
2021-04-01 20:08:48 +0900

    refactor: moved logic to api/Put.ts

--- src/api/Update.ts
+++ src/api/Update.ts
@@ -10,12 +10,14 @@ import { isDevMode } from "../utils";
 export const updateTaskData = async (
   id: number,
   task: ITask,
-  updatedTask: ITask
+  updatedTask: ITask,
+  tasks: ITask[],
+  setTasks: Function
 ) => {
   const url: string = isDevMode()
     ? API_URL + id
     : `${API_URL}?id=eq.${id}&${API_KEY}`;
-  return await fetch(url, {
+  const res: Response = await fetch(url, {
     method: "PUT",
     headers: {
       "Content-type": "application/json",
@@ -24,4 +26,25 @@ export const updateTaskData = async (
     },
     body: JSON.stringify(updatedTask),
   });
+  if (isDevMode()) {
+    await res.json().then((data: ITask) => {
+      if (res.ok) {
+        setTasks(
+          tasks.map((task) =>
+            task.id === id ? { ...task, reminder: data.reminder } : task
+          )
+        );
+      }
+    });
+  } else {
+    await res.json().then((data: ITask[]) => {
+      if (res.ok) {
+        setTasks(
+          tasks.map((task) =>
+            task.id === id ? { ...task, reminder: data[0].reminder } : task
+          )
+        );
+      }
+    });
+  }
 };
